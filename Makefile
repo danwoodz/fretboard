2fa3c0417687e0a470f672c8d981ec1394f24104
Dan Woods <dan.woods@socotra.com>
2020-12-29 17:48:33 -0800

    v1 seems to work

---
+++ Makefile
@@ -0,0 +1,5 @@
+fretboard: fretboard.c
+	echo "Building fretboard"
+	gcc -o fretboard fretboard.c
+
+
--- fretboard.c
+++ fretboard.c
@@ -1,1 +1,52 @@
 /* woohoo! */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+#define MAXFRET 12
+#define MINFRET 3
+#define NUMKEYS 12
+#define NUMMODES 6
+
+int main(int argc, char **argv) {
+
+	const char* const keys[] = {
+		"A", "A",
+		"A-sharp", "B-flat",
+		"B", "B",
+		"C", "C",
+		"C-sharp", "D-flat",
+		"D", "D",
+		"D-sharp", "E-flat",
+		"E", "E",
+		"F", "F",
+		"F-sharp", "G-flat",
+		"G", "G",
+		"G-sharp", "A-flat"
+	};
+
+
+	const char* const modes[] = {
+		"Ionian",
+		"Dorian",
+		"Phygian",
+		"Lydian",
+		"Mixolydian",
+		"Aelian"
+	};
+
+	printf("Find this scale:\n");
+
+	srand(time(NULL));
+	int key = rand() % (2 * NUMKEYS);
+	int mode = rand() % NUMMODES;
+	int fret = MINFRET + rand() % (1 + MAXFRET - MINFRET);
+
+	printf("Key: %s\n", keys[key]);
+	printf("Mode: %s\n", modes[mode]);
+	printf("Including Fret: %d\n", fret);
+
+
+	return 0;
+}
